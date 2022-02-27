01f047299c70f1966f9c097e770b4430d8a77002
Betty <betty_elfagid@yahoo.co.uk>
2022-02-27 23:35:05 +0000

    import the created components in ChatFeed.js and destructure the props.

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -1,5 +1,9 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
 const ChatFeed = (props) => {
- console.log(props)
+  const { chats, activeChat, userName, messages} = props;
  return(
      <div>
          ChatFeed
